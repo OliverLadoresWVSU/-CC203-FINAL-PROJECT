a1deee5a8e4ddda9a73f9ec433076745acacfe46
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2023-04-13 23:57:54 +0800

    Created a file test for the modules and updated the todo file

---
+++ filestructure.cpp
@@ -0,0 +1,14 @@
+// Using a temporary filename in order to conceptualize the initial idea of the program.
+// Based on the ideas I made while riding the jeepney
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream myfile;
+    myfile.open ("temp.txt");
+    myfile << "Writing this to a file.
+}  
