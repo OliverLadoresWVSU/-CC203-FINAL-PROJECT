3ab2a4e5f5bc97d1bec5118e4f55f98f03672b19
Oliver <oliver.ladores@wvsu.edu.ph>
2023-04-24 20:26:27 +0800

    Added a few member variables to Student class.

--- student.cpp
+++ student.cpp
@@ -14,7 +14,7 @@
 
 class Student {
 private:
-  std::optional<std::string> id, name, gender, course, year_and_section, email,
+  std::optional<std::string> id, last_name, first_name, middle_name, suffix, gender, course, year_and_section, email,
       phone_number, notes;
 
 public:
@@ -24,19 +24,22 @@ public:
    * All arguments default to std::nullopt when no input is given.
    */
   Student(const std::optional<std::string> idInput = std::nullopt,
-          const std::optional<std::string> nameInput = std::nullopt,
+          const std::optional<std::string> last_nameInput = std::nullopt,
+          const std::optional<std::string> first_nameInput = std::nullopt,
+          const std::optional<std::string> middle_nameInput = std::nullopt,
+          const std::optional<std::string> suffixInput = std::nullopt,
           const std::optional<std::string> genderInput = std::nullopt,
           const std::optional<std::string> courseInput = std::nullopt,
           const std::optional<std::string> year_and_sectionInput = std::nullopt,
           const std::optional<std::string> emailInput = std::nullopt,
           const std::optional<std::string> phone_numberInput = std::nullopt,
           const std::optional<std::string> notesInput = std::nullopt)
-      : id(idInput), name(nameInput), gender(genderInput), course(courseInput),
+      : id(idInput), last_name(last_nameInput), first_name(first_nameInput), middle_name(middle_nameInput), suffix(suffixInput), gender(genderInput), course(courseInput),
         year_and_section(year_and_sectionInput), email(emailInput),
         phone_number(phone_numberInput), notes(notesInput) {}
 
   /**
-   * @brief Sets the members based on the arguments.
+   * @brief Sets the members based on the arguments. We have to implement it this way cause we want to access the private members.
    *
    * @param member The member to modify.
    * @param data The data to set the member to.
@@ -44,8 +47,8 @@ public:
   void set(std::string member, std::optional<std::string> data) {
     if (member.compare("id") == 0)
       id = data;
-    else if (member.compare("name") == 0)
-      name = data;
+    else if (member.compare("first_name") == 0)
+      first_name = data;
   }
 };
 
@@ -54,6 +57,6 @@ auto main() -> int {
   auto Manuel = Student();
 
   Ollie.set("id", "2022M1111");
-  Manuel.set("name", "John Manuel Carado");
+  Manuel.set("first_name", "John Manuel Carado");
   return 0;
 }
